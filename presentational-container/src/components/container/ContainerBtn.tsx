1acb9f7273acce69e8bcc209866405dba3362416
benjuwan <benjuwan.banquet12@gmail.com>
2023-10-17 16:26:57 +0900

    表象コンポとコンテナコンポの試用ボタンUIを追加.

---
+++ presentational-container/src/components/container/ContainerBtn.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { PresentationalBtn } from "../presentational/PresentationalBtn";
+
+// Container Component：表象的なコンポーネント（Presentational Component）を包含（import）したコンテナのようにそれを入れて動かすコンポーネント
+export const ContainerBtn = () => {
+    // consoleMethod
+    const consoleMethod = () => {
+        console.log('コンソールにダンプ');
+    }
+
+    // alartMethod
+    const alartMethod = () => {
+        alert('アラートに出力');
+    }
+
+    // FizzBuzz
+    const [fizzBuzzState, setFizzBuzzState] = useState<string>('');
+    const fizzBuzz = (setNumber: number) => {
+        const numValue = Math.floor(Math.random() * setNumber);
+        let subjectTxt: string = '';
+
+        // numValue % 15 === 0 
+        if (
+            numValue % 3 === 0 &&
+            numValue % 5 === 0
+        ) {
+            console.log(`FizzBuzz：${numValue}`);
+            subjectTxt = 'FizzBuzz';
+        } else if (numValue % 5 === 0) {
+            console.log(`Fizz：${numValue}`);
+            subjectTxt = 'Fizz';
+        } else if (numValue % 3 === 0) {
+            console.log(`Buzz：${numValue}`);
+            subjectTxt = 'Buzz';
+        } else {
+            console.log(`noFizzBuzz：${numValue}`);
+            subjectTxt = 'noFizzBuzz';
+        }
+
+        // return `${subjectTxt}:${numValue}`;
+        setFizzBuzzState((prevState) => `${subjectTxt}:${numValue}`);
+    }
+
+    return (
+        <>
+            <PresentationalBtn btnTxt={fizzBuzzState.length <= 0 ? 'fizzBuzz問題' : fizzBuzzState} method={() => {
+                fizzBuzz(100);
+            }} anyClassName="anyClassName_001" />
+
+            <PresentationalBtn method={undefined} />
+
+            <PresentationalBtn btnTxt="コンソールダンプ" method={consoleMethod} anyClassName="anyClassName_003" />
+
+            <PresentationalBtn btnTxt="アラート" method={alartMethod} anyClassName="anyClassName_002" />
+        </>
+    );
+}
---
+++ presentational-container/src/components/presentational/PresentationalBtn.tsx
@@ -0,0 +1,66 @@
+import { ReactNode, FC } from "react";
+import styled from "styled-components";
+
+type btnProps = {
+    btnTxt?: string | HTMLElement | ReactNode;
+    method: undefined | (() => void); // ユニオンで使用する場合、関数の型の表記はかっこで囲む必要がある。() => void → (() => void)
+    anyClassName?: string;
+}
+
+// Presentational Component：表象的なコンポーネント
+export const PresentationalBtn: FC<btnProps> = (props) => {
+    const { btnTxt, method, anyClassName } = props;
+    return (
+        <Btn
+            type="button"
+            onClick={method}
+            className={anyClassName ? `${anyClassName}` : 'defaultStyle'}
+        >
+            {btnTxt ? `${btnTxt}` : 'ただのボタン'}
+        </Btn>
+    );
+}
+
+const Btn = styled.button`
+display: block;
+cursor: pointer;
+appearance: none;
+border-radius: 4px;
+border: unset;
+width: clamp(160px, calc(100vw/2), 320px);
+margin: 1em auto;
+line-height: 2;
+color: #fff;
+
+    &.defaultStyle {
+        background-color: #6e6e6e;
+        border-bottom: 3px solid #333;
+    }
+
+    &.anyClassName_001 {
+        background-color: #fff;
+        color: #333;
+        border: 1px solid #333;
+        border-bottom: 3px solid #333;
+
+        &:hover {
+            border-bottom: 1px solid #333;
+        }
+    }
+
+    &.anyClassName_002 {
+        background-color: #a00f0f;
+        border-bottom: 3px solid #370606;
+    }
+
+    &.anyClassName_003 {
+        background-color: #0d1893;
+        border-bottom: 3px solid #110637;
+    }
+
+    &:hover {
+        transition: all.25s;
+        border-bottom-color: transparent;
+        transform: translateY(3px);
+    }
+`;
