573daee6f27c950cab5d8d58791f0cbf73be59ba
benjuwan <benjuwan.banquet12@gmail.com>
2023-11-25 15:23:06 +0900

    READMEの修正とRedux の action/reducer のサンプルファイルを追加.

---
+++ redux/stores/reducers.ts
@@ -0,0 +1,39 @@
+import type { Reducer } from 'redux';
+import { CounterActions as Action } from './actions';
+import type { CounterAction } from './actions';
+
+export interface CounterState {
+  count: number;
+}
+export const initialState: CounterState = { count: 0 };
+
+export const counterReducer: Reducer<CounterState, CounterAction> = (
+  state = initialState,
+  action
+) => {
+  if (action.type === Action.DECREMENT) {
+    return {
+      ...state,
+      count: state.count < 1 ? 0 : state.count - 1,
+    };
+  } 
+  
+  else if (action.type === Action.INCREMENT) {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  } 
+  
+  else if (action.type === Action.ADD) {
+    return {
+      ...state,
+      count: state.count + (action.amount ?? 0),
+    };
+  } 
+  
+  else {
+    const _: never = action.type;
+    return state;
+  }
+};
